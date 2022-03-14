69586132855dc830d5ca06a3d23884e7c7f9c539
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-14 15:08:37 +0500

    add render and addbook

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-books = {
-  id: "",
-  title: "",
-  author: "",
-};
 const addBookBtn = document.querySelector(".add-book-btn");
-
 const form = document.querySelector(".form");
-addBookBtn.addEventListener("click", () => {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  console.log("Add clicked");
-  console.log(title + " and " + author);
-  books.title= title;
-  books.author=author;
+const listItem = document.querySelector('.books-list');
+
+// books = {
+//   id: "",
+//   title: "",
+//   author: "",
+// };
+
+booksList = [];
+
+function addBook(book) {
+  booksList.push(book);
+}
+
+const renderBook  = () => {
+  listItem.innerHTML = '';
+
+  booksList.forEach((book) => {
+    listItem.innerHTML += `
+          <li>
+            <p>${book.title}</p>
+            <p>${book.author}</p>
+            <button class="removeBtn">Remove</button>
+          </li>
+    `
+  })
+    
+}; 
+
+
+addBookBtn.addEventListener("click", (e) => {
+  // const title = document.getElementById("title").value;
+  // const author = document.getElementById("author").value;
+  // console.log("Add clicked");
+  // console.log(title + " and " + author);
+  // books.title= title;
+  // books.author=author;
+  // e.preventDefault();
+
+  const title = form.title.value;
+  const author = form.author.value;
+
+  form.title.value = '';
+  form.author.value = '';
+  
+  addBook({ title, author })
+  renderBook();
   
 });
